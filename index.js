19ac05724adc969f98b4c11ce63dbec39adebd9d
weitenghuang <weiteng.huang@concur.com>
2015-12-21 11:26:41 -0500

    add http :3000

---
+++ index.js
@@ -0,0 +1,6 @@
+var http = require('http');
+var os = require('os');
+http.createServer(function printServerName(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, you land on ' + os.hostname());
+}).listen(3000);
