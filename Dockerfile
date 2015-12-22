85c68c6594939983fcbbb32fa3f26271ef041baf
weitenghuang <weiteng.huang@concur.com>
2015-12-21 19:21:38 -0500

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,15 @@
-FROM node:4-onbuild
+FROM node:4.2.3
+
+RUN apt-get update
+
+RUN mkdir -p /var/site
+
+ADD . /var/site
+
+WORKDIR /var/site
+
+RUN npm install
+
 EXPOSE 3030
+
+CMD npm start
--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 var os = require('os');
 http.createServer(function printServerName(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, you land on ' + os.hostname());
+  res.end('Hello world!!!, you land on ' + os.hostname());
 }).listen(3030);
